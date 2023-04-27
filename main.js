5a4426dde7132a943893ab6a877e10af95286689
Tashi Freyer <tashi.freyer@student.uibk.ac.at>
2023-04-27 17:26:44 +0200

    added photo  icons for sights

--- main.js
+++ main.js
@@ -122,7 +122,13 @@ async function showSights(url) {
     let jsondata = await response.json();
     L.geoJSON(jsondata, {
         pointToLayer: function(feature, latlng) {
-            return L.marker(latlng);
+            return L.marker(latlng,{
+                icon:L.icon({
+                    iconUrl: 'icons/photo.png',
+                    iconAnchor: [16, 37],
+                    popupAnchor: [0, -37]
+                })
+            });
         },
         onEachFeature: function (feature, layer) {
             let prop = feature.properties;
